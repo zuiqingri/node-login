f60172cd2054a3a409f647831ca686efbee60821
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-12-24 23:52:15 +0800

    SignUp by fs module

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -20,10 +20,14 @@ class User {
     return { success: false, msg: "There is not ID" };
   }
 
-  register() {
+  async register() {
     const client = this.body;
-    const response = UserStorage.save(client);
-    return response;
+    try {
+      const response = await UserStorage.save(client);
+      return response;
+    } catch (err) {
+      return { success: false, msg: err };
+    }
   }
 }
 
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -14,8 +14,10 @@ class UserStorage {
     return userInfo;
   }
 
-  static getUsers(...fields) {
-    // const users = this.#users;
+  static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+    if (isAll) return users;
+
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -23,6 +25,15 @@ class UserStorage {
       return newUsers;
     }, {});
     return newUsers;
+  }
+
+  static getUsers(isAll, ...fields) {
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUsers(data, isAll, fields);
+      })
+      .catch(console.error);
   }
 
   static getUserInfo(id) {
@@ -34,11 +45,15 @@ class UserStorage {
       .catch(console.error);
   }
 
-  static save(userInfo) {
-    // const users = this.#users;
+  static async save(userInfo) {
+    const users = await this.getUsers(true);
+    if (users.id.includes(userInfo.id)) {
+      throw "exist already";
+    }
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.password.push(userInfo.password);
+    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
     return { success: true };
   }
 }
