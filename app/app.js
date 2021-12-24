d9c95c8ceef60798ab2dfe2713714832d9d19fba
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-12-24 17:00:19 +0800

    Front javascript

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 //App setting
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); //user-> register middleware
 
