73a8f7d40d075dcb956345a8a7376b7189a9f4e3
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-12-25 13:21:19 +0800

    winston log

--- app/app.js
+++ app/app.js
@@ -4,12 +4,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 dotenv.config();
 
 const app = express();
 
+// const accessLogStream = require("./src/config/log");
+
 //routing
 const home = require("./src/routes/home");
+
+// const logger = require("./src/config/logger");
+// logger.log("info", "Hello my frient");
+// logger.info("Hello!");
+// logger.error("Hello!");
 
 //App setting
 app.set("views", "./src/views");
@@ -18,6 +26,8 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(morgan("dev"));
+// app.use(morgan("common", { stream: accessLogStream }));
 
 app.use("/", home); //user-> register middleware
 
--- app/bin/www.js
+++ app/bin/www.js
@@ -1,8 +1,10 @@
 "use strict";
 
 const app = require("../app");
+const logger = require("../src/config/logger");
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("server:3000");
+  logger.info(`Server running in PORT:${PORT}`);
 });
